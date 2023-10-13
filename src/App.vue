8fce11b93e6664f812376a0f94ecab87fa1fb677
Primexz <johann@pepebot.info>
2023-10-13 10:52:54 +0200

    feat: add prettier

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+    import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <a class="text-red-800">adasd</a>
+    <a class="text-red-800">adasd</a>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+    .logo {
+        height: 6em;
+        padding: 1.5em;
+        will-change: filter;
+        transition: filter 300ms;
+    }
+    .logo:hover {
+        filter: drop-shadow(0 0 2em #646cffaa);
+    }
+    .logo.vue:hover {
+        filter: drop-shadow(0 0 2em #42b883aa);
+    }
 </style>
