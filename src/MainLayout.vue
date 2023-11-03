946da5883058e45ea5f867a66b32a91c6db19520
Primexz <johann@pepebot.info>
2023-11-03 08:55:09 +0100

    feat: add hacky landing page

--- src/MainLayout.vue
+++ src/MainLayout.vue
@@ -1,7 +1,7 @@
-<script setup>
-    import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
     <router-view />
 </template>
+
+<script setup>
+    import HelloWorld from './components/HelloWorld.vue'
+</script>
--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,5 +1,25 @@
 <template>
-    <p>Hello World ^^</p>
+    <div class="min-h-screen flex flex-col">
+        <header class="p-4 bg-blue-500 text-white">
+            <nav>
+                <ul class="flex space-x-4">
+                    <li><a href="#" class="hover:underline">Link 1</a></li>
+                    <li><a href="#" class="hover:underline">Link 2</a></li>
+                    <li><a href="#" class="hover:underline">Link 3</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main class="flex-grow p-4">
+            <h1 class="text-2xl font-bold">Website Titel</h1>
+        </main>
+
+        <footer class="p-4 bg-blue-500 text-white">
+            <p>Copyright &copy; 2022</p>
+        </footer>
+    </div>
+
+    <!-- <p>Hello World ^^</p> -->
 </template>
 
 <script setup lang="ts"></script>
