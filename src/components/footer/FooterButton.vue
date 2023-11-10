4bd7f825f2491ee68c8152d7d99dca2ab4a178c7
Primexz <johann@pepebot.info>
2023-11-10 09:29:57 +0100

    feat: footer buttons

---
+++ src/components/footer/FooterButton.vue
@@ -0,0 +1,12 @@
+<template>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition">
+        {{ props.text }}
+    </button>
+</template>
+
+<script setup lang="ts">
+    const props = defineProps({
+        text: { type: String, required: true },
+        route: { type: String, required: true },
+    })
+</script>
