f279fe333720c973a0c57257d0672169f8a1f01f
Luca Albrecht <albrecht@devlabor.com>
2023-11-10 08:24:56 +0100

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,26 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {
-            margin: {
-                xxl: '11rem',
-                l: '7rem',
-            },
+        colors: {
+            accent: '#3A688D',
+            gray: '#2A2A2A',
+
+            // dark mode
+            white: '#F2F2F2',
+            light_gray: '#393939',
+
+            // light mode
+            beige: '#C3B98E',
+            light_beige: '#DDD199',
         },
+        margin: {
+            xxl: '12rem',
+            l: '7rem',
+        },
+        fontFamily: {
+            sans: ['"Source Sans 3"', 'sans-serif'],
+        },
+        extend: {},
     },
     plugins: [],
 }
