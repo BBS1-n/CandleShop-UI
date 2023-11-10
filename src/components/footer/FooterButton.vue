d85b6985a2f6e39b6d4e3226fc4fbe8cca05b3d0
Primexz <johann@pepebot.info>
2023-11-10 09:57:55 +0100

    feat(footer_button): route on button click

--- src/components/footer/FooterButton.vue
+++ src/components/footer/FooterButton.vue
@@ -1,10 +1,21 @@
 <template>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition">
+    <button
+        @click="route(props.route)"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition"
+    >
         {{ props.text }}
     </button>
 </template>
 
 <script setup lang="ts">
+    import { useRouter } from 'vue-router'
+
+    const router = useRouter()
+
+    function route(route: string) {
+        router.push(route)
+    }
+
     const props = defineProps({
         text: { type: String, required: true },
         route: { type: String, required: true },
