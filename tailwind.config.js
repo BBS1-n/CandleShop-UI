e0274678332ff1258ebf2576658be059a384ddb9
Primexz <johann@pepebot.info>
2023-11-10 08:40:09 +0100

    Revert "feat: color variables config (#3)"
    This reverts commit 65c5e42a91239851481a3c99913427dc2c77b5dd.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,11 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        colors: {
-            accent: '#3A688D',
-            gray: '#2A2A2A',
-
-            // dark mode
-            white: '#F2F2F2',
-            light_gray: '#393939',
-
-            // light mode
-            beige: '#C3B98E',
-            light_beige: '#DDD199',
-        },
-        margin: {
-            xxl: '12rem',
-            l: '7rem',
-        },
-        fontFamily: {
-            sans: ['"Source Sans 3"', 'sans-serif'],
+        extend: {
+            margin: {
+                xxl: '11rem',
+                l: '7rem',
+            },
         },
     },
     plugins: [],
