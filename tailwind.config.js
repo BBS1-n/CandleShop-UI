128a72c8b5034ee17975decc5df7e8297173cab0
Luca Albrecht <albrecht@devlabor.com>
2023-11-10 08:50:46 +0100

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,25 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        colors: {
-            accent: '#3A688D',
-            gray: '#2A2A2A',
+        extend: {
+            margin: {
+                xxl: '12rem',
+                l: '7rem',
+            },
+            colors: {
+                accent: '#3A688D',
+                gray: '#2A2A2A',
 
-            // dark mode
-            white: '#F2F2F2',
-            light_gray: '#393939',
+                // dark mode
+                white: '#F2F2F2',
+                light_gray: '#393939',
 
-            // light mode
-            beige: '#C3B98E',
-            light_beige: '#DDD199',
-        },
-        margin: {
-            xxl: '12rem',
-            l: '7rem',
-        },
-        fontFamily: {
-            sans: ['"Source Sans 3"', 'sans-serif'],
+                // light mode
+                beige: '#C3B98E',
+                light_beige: '#DDD199',
+            },
         },
     },
+
     plugins: [],
 }
