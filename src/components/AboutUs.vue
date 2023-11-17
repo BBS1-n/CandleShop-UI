3b846c577c3135e343e4a0ad4b0940a7f47bb64e
Primexz <johann@pepebot.info>
2023-11-17 09:11:12 +0100

    feat: add production

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -2,12 +2,10 @@
     <div class="text-center text-white mx-auto max-w-prose">
         <h1 class="text-4xl font-bold underline text-accent">Über Uns</h1>
         <p class="text-base mt-4">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-            sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Wir, die Mitglieder des Schülerunternehmens „Candleshop“, bestehen aus 14 engagierten Schülerinnen und Schülern der HA23B und nehmen unsere Tätigkeit mit Stolz im Rahmen unserer einjährigen Berufsfachschule Wirtschaft mit Schwerpunkt Handel an den Berufsbildenden Schulen I in Lüneburg wahr.
+Unser Hauptziel ist die Produktion und der Verkauf hochwertiger Kerzen, die an unserem Verkaufsstand in der Schule erhältlich sind. Unser einzigartiges Konzept hebt sich hervor, da wir sowohl regionales Bienenwachs als auch Sojawachs nutzen, um eine breite Palette von Kerzen anzubieten. Damit können wir auch veganen Kunden gerecht werden, die unsere Kerzen schätzen. Die Verwendung von lokal beschafftem Bienenwachs stärkt die Verbindung zur Region und unterstützt gleichzeitig Imker vor Ort.
+Wir laden Sie herzlich dazu ein, unsere Kerzen und unsere Philosophie während des anstehenden Winterzaubers in der Pausenhalle der BBS I Lüneburg am 05. Dezember 2023 zu erleben.
+Wir sind überzeugt, dass Sie von der Qualität unserer Kerzen und dem Engagement unseres Schülerunternehmens beeindruckt sein werden. Mit Ihrem Kauf unterstützen Sie nicht nur unsere Bildung, sondern tragen auch zur Förderung nachhaltiger und regionaler Produkte bei.
         </p>
     </div>
 </template>
---
+++ src/components/Production.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center text-white mx-auto max-w-prose">
+        <h1 class="text-4xl font-bold underline text-accent">Herstellung</h1>
+        <p class="text-base mt-4">
+            Wir sind toll
+          </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
