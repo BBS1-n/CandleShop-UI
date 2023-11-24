30a4c361dd17c42d20171dd987626a1c32426d47
Cornect <simon.steckelberg@gmail.com>
2023-11-24 09:50:14 +0100

    added text for herstellung

--- src/components/Production.vue
+++ src/components/Production.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="text-center mx-auto max-w-prose">
         <h1 class="text-4xl font-bold underline text-accent">Herstellung</h1>
-        <p class="text-base mt-4 text-[#2A2A2A] dark:text-[#F2F2F2]">Wir sind toll</p>
+        <p class="text-base mt-4 text-[#2A2A2A] dark:text-[#F2F2F2]">
+            Unsere Kerzen bestehen aus Sojawachs und werden in liebevoller Handarbeit hergestellt. Dafür wird das Wachs
+            auf unseren Induktionskochplatten erhitzt und eingefärbt. Dann folgt das Gießen in die Gefäße oder Formen.
+            Nach 48 Stunden sind die Kerzen vollständig ausgekühlt und können aus den Formen herausgelöst werden. Fertig
+            sind die veganen und von uns Schülern*innen handgefertigten Kerzen.
+        </p>
     </div>
 </template>
 
