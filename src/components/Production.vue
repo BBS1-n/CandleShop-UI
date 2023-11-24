d1cd49115acd22650c722fc8b5cff97d7289cd19
Primexz <johann@pepebot.info>
2023-11-24 11:05:44 +0100

    fix: make about us and production responsive

--- src/components/Production.vue
+++ src/components/Production.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-center mx-auto max-w-prose">
+    <div class="text-center mx-auto max-w-sm lg:max-w-prose">
         <h1 class="text-4xl font-bold underline text-accent">Herstellung</h1>
         <p class="text-base mt-4 text-[#2A2A2A] dark:text-[#F2F2F2]">
             Unsere Kerzen bestehen aus Sojawachs und werden in liebevoller Handarbeit hergestellt. Dafür wird das Wachs
