b41efa1e0a20754df641947ad104c6da6e738cd4
Cornect <simon.steckelberg@gmail.com>
2023-11-30 22:33:22 +0100

    fix: on mobile less cards

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,9 +2,9 @@
     <div id="products" class="text-center text-white mx-auto max-w-3xl">
         <h1 class="text-4xl font-bold underline text-accent">Produkte</h1>
 
-        <carousel class="mt-4" :autoplay="parseInt('2000')" :itemsToShow="3.95" :wrapAround="true" :transition="500">
+        <carousel class="mt-4" :autoplay="5000" :pauseAutoplayOnHover="true" :itemsToShow="numOfProducts" :wrapAround="true">
             <Slide v-for="product in productList" :key="product.price">
-                <div class="max-w-sm rounded overflow-hidden shadow-lg bg-[#C3B98E] dark:bg-[#393939]">
+                <div class="carousel__item max-w-sm rounded overflow-hidden shadow-lg bg-[#C3B98E] dark:bg-[#393939]">
                     <img class="w-full p-2" :src="product.image" :alt="product.description" />
                     <div class="px-6 py-4">
                         <div class="font-bold text-xl mb-2 text-gray dark:text-white">
@@ -35,6 +35,31 @@
         image: string
     }
 
+
+    // müll will nicht
+    //const getNumOfProducts = () => {
+    //    let windowWidth = ref({w: window.innerWidth, h: window.innerHeight})
+
+    //    const onResizeEvent = () => {
+    //        console.log("ress");
+    //        windowWidth.value.w = window.innerWidth
+    //    }
+    //    onMounted(() => window.addEventListener('resize', onResizeEvent))
+    //    onUnmounted(() => window.removeEventListener('resize', onResizeEvent))
+
+    //    const numOfProducts = computed(() => {
+    //      return windowWidth.value.h > windowWidth.value.w ? 1.9 : 3.5;
+    //    })
+
+    //    const width = computed(() => windowWidth.value.w)
+    //    const height = computed(() => windowWidth.value.h)
+    //    console.log("resize");
+    //    return { width, height, numOfProducts }
+    //}
+
+    let isMobile = window.innerHeight > window.innerWidth;
+    let numOfProducts = isMobile ? 1.9 : 3.5;
+
     const productList: Product[] = [
         { id: 1, description: 'Bubble Kerze', price: '6€', image: kerze_bubble },
         { id: 2, description: 'Tannenbaum Kerze', price: '6€', image: kerze_tannenbaum },
@@ -47,10 +72,20 @@
 <style>
     .carousel__item {
         pointer-events: none;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        width: 100%;
+        min-height: 100%;
+        height: 100% !important;
     }
 
     .carousel__slide {
         padding: 5px;
+        min-height: 200px;
+        width: 100%;
+        height: 100%;
+        scale: 0.9;
     }
 
     .carousel__viewport {
@@ -65,27 +100,16 @@
         transition: 0.5s;
     }
 
-    .carousel__slide {
-        opacity: 0.9;
-        transform: rotateY(-20deg) scale(0.9);
+    .carousel__item img {
+        height: 250px !important;
+        width: 100% !important;
+        object-fit: cover;
     }
-
-    .carousel__slide--active ~ .carousel__slide {
-        transform: rotateY(20deg) scale(0.9);
-    }
-
-    .carousel__slide--prev {
-        opacity: 1;
-        transform: rotateY(-10deg) scale(0.95);
-    }
-
-    .carousel__slide--next {
-        opacity: 1;
-        transform: rotateY(10deg) scale(0.95);
+    .carousel__item > div {
+        padding: 5px;
     }
 
     .carousel__slide--active {
-        opacity: 1;
-        transform: rotateY(0) scale(1.1);
+        scale: 1;
     }
 </style>
